d810a5f03e6eb826f4e4a189696790257c4e4969
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2025-02-12 21:23:26 -0800

    Added more stuff to lab3
    Signed-off-by: 64815328+Eutropios@users.noreply.github.com

--- Walkthroughs/JavaScript-Walkthrough/js/script.js
+++ Walkthroughs/JavaScript-Walkthrough/js/script.js
@@ -1,58 +1,55 @@
-(function () {
-    var setCopyRightYear = function () {
-        var copy = document.querySelector("footer>kbd>span");
-        if (copy !== null)
-            copy.textContent = new Date().getFullYear().toString();
+(() => {
+    const setCopyRightYear = ()=> {
+        const copy = document.querySelector("footer>kbd>span");
+        if (copy !== null) copy.textContent = new Date().getFullYear().toString();
     };
-    var changeBackgroundColor = function () {
-        var first = document.getElementById("first");
-        if (first !== null)
-            first.style.background = "AliceBlue";
-        var second = document.getElementById("second");
-        if (second !== null)
-            second.style.background = "Aquamarine";
-        var third = document.getElementById("third");
-        if (third !== null)
-            third.style.background = "DarkSalmon";
+
+    const changeBackgroundColor = ()=> {
+        const first = document.getElementById("first");
+        if (first !== null) first.style.background = "AliceBlue";
+        const second = document.getElementById("second");
+        if (second !== null) second.style.background = "Aquamarine";
+        const third = document.getElementById("third");
+        if (third !== null) third.style.background = "DarkSalmon";
     };
-    var setValues = function () {
-        var dropdown = document.querySelector("select");
+
+    const setValues = ()=> {
+        const dropdown = document.querySelector("select");
         if (dropdown !== null) {
-            for (var i = 1; i < 1001; ++i) {
-                var option = document.createElement("option");
+            for (let i = 1; i < 1001; ++i) {
+                const option = document.createElement("option");
                 option.value = i.toString();
                 option.text = i.toString();
                 dropdown.add(option);
             }
         }
     };
-    var convertNumber = function (value) {
+
+    const convertNumber = (value) => {
         console.log(value);
-        var table = document.getElementById("output");
-        var tbody = document.createElement("tbody");
-        var tr = document.createElement("tr");
-        var data = [];
+        const table = document.getElementById("output");
+        const tbody = document.createElement("tbody");
+        const tr = document.createElement("tr");
+        const data = [];
         data.push(value.toString());
         data.push(value.toString(2));
         data.push(value.toString(16));
-        for (var i = 0; i < data.length; ++i) {
-            var td = document.createElement("td");
+        for (let i = 0; i < data.length; ++i) {
+            const td = document.createElement("td");
             td.setAttribute("class", "text-center");
-            var cell = document.createTextNode(data[i]);
+            const cell = document.createTextNode(data[i]);
             td.appendChild(cell);
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
-        if (table !== null)
-            table.appendChild(tbody);
+        if (table !== null) table.appendChild(tbody);
     };
-    window.onload = function () {
+
+    window.onload = ()=> {
         setCopyRightYear();
-        // @ts-ignore
         document.querySelector("#change").onclick = changeBackgroundColor;
         setValues();
-        var dropdown = document.querySelector("select");
-        // @ts-ignore
-        dropdown.onchange = function (event) { return convertNumber(Number(event.target.value)); };
+        const dropdown = document.querySelector("select");
+        dropdown.onchange = (event) => convertNumber(Number(event.target.value));
     };
 })();
