4083b51c4cd4087de089eb6b5badc9a96a2510de
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2025-03-03 16:51:09 -0800

    Continue work on Lab 5
    Continue work on Lab 5
    
    Signed-off-by: <64815328+Eutropios@users.noreply.github.com>

--- Labs/Lab5/js/weather.js
+++ Labs/Lab5/js/weather.js
@@ -5,11 +5,27 @@
     };
 
     const getGeoLocation = () => {
-        // your code goes here
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const xy = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                };
+                console.log(`(Latitude,Longitude)=(${xy.latitude},${xy.longitude})`);
+            });
+        } else {
+            console.log("\t|Geolocation is not supported by this browser!");
+        }
     };
 
     const getJSONData = async (url) => {
-        // your code goes here
+        try {
+            const response = await window.fetch(url);
+            const data = response.json();
+            return data;
+        } catch (error) {
+            console.log("\t|Something went wrong while fetching JSON data!");
+        }
     };
 
     const setWeatherIcon = (description) => {
@@ -20,12 +36,38 @@
         // your code goes here
     };
 
+    const createCitySelect = (select) => {
+        const cityPlaceholder = document.createElement("option");
+        select.remove(0);
+        cityPlaceholder.text = "Select a city";
+        cityPlaceholder.hidden = true;
+        select.add(cityPlaceholder);
+    };
+
     const populateCities = async () => {
-        // your code goes here
+        const countrySelect = document.getElementById("countryselect").value;
+        const citySelect = document.getElementById("cityselect");
+        const data = await getJSONData("./data/cities.json");
+        const cities = data[countrySelect];
+        createCitySelect(citySelect);
+        for (i of cities) {
+            const option = document.createElement("option");
+            option.value = i.toString();
+            option.text = i.toString();
+            citySelect.add(option);
+        }
     };
 
     const populateCountries = async () => {
-        // your code goes here
+        const data = await getJSONData("./data/cities.json");
+        const countries = Object.keys(data);
+        const dropdown = document.getElementById("countryselect");
+        for (i of countries) {
+            const option = document.createElement("option");
+            option.value = i.toString();
+            option.text = i.toString();
+            dropdown.add(option);
+        }
     };
 
     const displayMap = () => {
@@ -34,5 +76,8 @@
 
     window.onload = () => {
         setCopyRightYear();
+        getGeoLocation();
+        populateCountries();
+        document.getElementById("weatherData").onclick = populateCities;
     };
 })();
